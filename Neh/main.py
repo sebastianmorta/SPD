e579501a4d0388ae5c4760f7c58de2d70b5cab10
sebastianmorta <mr.mchaker@gmail.com>
2020-05-25 18:49:19 +0200

    neeeh

--- Neh/main.py
+++ Neh/main.py
@@ -1,6 +1,5 @@
 import numpy
 
-
 def ReadFile(FileName):
     data = []
     with open('data\\NEH' + str(FileName) + '.DAT') as file:
@@ -9,13 +8,8 @@ def ReadFile(FileName):
         print(n, m)
         for i in file:
             if not i.isspace():
-                #data.append(Task(i, [int(x) for x in i.split()]))
                 data.append([int(x) for x in i.split()])
-
-            #
-            #     p, w, d = i.split()
-            #     data.append(Task(int(p), int(w), int(d)))
-    return data,n,m
+    return data, n, m
 
 #j-zadanie k-maszyna
 #n-zadania m-maszyny
@@ -38,7 +32,7 @@ def sumList(data, n, m):
         sumlist.append(i)
     return sorted(sumlist, key=lambda x: sum(data, x, m), reverse=True)
 
-def inserter(sequence,index,value):
+def inserter(sequence, index, value):
     new_sequence = sequence[:]
     new_sequence.insert(index, value)
     return new_sequence
@@ -46,36 +40,27 @@ def inserter(sequence,index,value):
 def Neh(data, n, m):
     CTab = Cmax(data, n, m)
     PTab = sumList(data, n, m)
-    current_sequence =[]
+    current_sequence = [PTab[0]]
     current_sequence.append(PTab[0])
-    for i in range(1,n):
-        cm=float("inf")
-        for j in range(i+1):
+    for i in range(1, n):
+        cm = float("inf")
+        for j in range(0, i + 1):
             temp_sequence = inserter(current_sequence, j, PTab[i])
-            temp_cmax = Cmax(data, n, m)
-            if cm > temp_cmax[len(temp_sequence)-1][m]:
+            temp_cmax = Cmax(data, len(temp_sequence), m)[len(temp_sequence)-1][m]
+            if cm > temp_cmax:
                 best_sequence = temp_sequence
-                cm = temp_cmax[len(temp_sequence)][m-1]
+                cm = temp_cmax
             current_sequence = best_sequence
-    return current_sequence
+    return  current_sequence
 
-
-
-
-
-
-
-# def read_wyniki(nb):
-#     with open('data\\wyniki' + str(nb) + '.DAT') as f:
-#         return int(f.readline())
-
-
+def read_wyniki(nb):
+    with open('data\\wyniki' + str(nb) + '.DAT') as f:
+        return int(f.readline())
 
 if __name__ == '__main__':
     result_tab = []
     data, n, m = ReadFile(1)
     C = Cmax(data, int(n), int(m))
-
-    print(Neh(data,int(n),int(m)))
+    print(Neh(data, int(n), int(m)))
     print('wynikiWITI: ')
     print(data)
