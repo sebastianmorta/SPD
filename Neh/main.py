df8bc86cb8889b49b0b4706953cddbd369d709e1
sebastianmorta <mr.mchaker@gmail.com>
2020-05-27 11:07:34 +0200

    nehaf

--- Neh/main.py
+++ Neh/main.py
@@ -1,12 +1,9 @@
 import numpy
-import sys
 
 def ReadFile(FileName):
     data = []
     with open('data\\NEH' + str(FileName) + '.DAT') as file:
-        #n, m = [int(x) for x in next(file).split()]
         n, m = file.readline().split()
-        #print(n, m)
         for i in file:
             if not i.isspace():
                 data.append([int(x) for x in i.split()])
@@ -21,78 +18,55 @@ def Cmax(data, n, m):
             C[j][k] = max(C[j-1][k], C[j][k-1]) + data[j-1][k-1]
     return C
 
-def sorted_by_p(data, n, m):
+def SumSortP(data, n, m):
     permutation = []
     result = []
     for i in range(n):
         permutation.append(i)
-    temp = sorted(permutation, key=lambda x: sum(data, x, m), reverse=True)
+    temp = sorted(permutation, key=lambda x: Psum(data, x, m), reverse=True)
     for i in temp:
         result.append(data[i])
     return result
 
-def sum(data, n, m):
+def Psum(data, n, m):
     sumP = 0
     for i in range(m):
         sumP += data[n][i]
     return sumP
 
-def sumList(data, n, m):
-    sumlist=[]
-    for i in range(n):
-        sumlist.append(i)
-    return sorted(sumlist, key=lambda x: sum(data, x, m), reverse=True)
-
-def inserter(sequence, index, value):
-    new_sequence = sequence[:]
-    print('newsewfa', new_sequence,index,value)
-    new_sequence.insert(index, value)
-    return new_sequence
-
-def neh(data, n, m):
-    tasks_lst = sorted_by_p(data, n, m)
-    current_task = tasks_lst[0]
-    sequence = [current_task]
+def Neh(data, n, m):
+    SortedList = SumSortP(data, n, m)
+    CurrentTask = SortedList[0]
+    sequence = [CurrentTask]
     best_sequence = []
     for i in range(1, n): # dla każdego zadania
-        best_c_max = float("inf") # bardzo duża liczba
-        for j in range(0, i+1): # dla każdej pozycji w permutacji
-            temp_sequence = list.copy(sequence)
-            temp_sequence.insert(j, tasks_lst[i])
+        BestCmax = float("inf") # bardzo duża liczba
+        for j in range(i+1): # dla każdej pozycji w permutacji
+            temp_sequence = sequence[:]
+            temp_sequence.insert(j, SortedList[i])
             n = len(temp_sequence)
-            c_max_seq = Cmax(temp_sequence, n, m)[n][m]
-            if c_max_seq < best_c_max:
+            CurrentCmax = Cmax(temp_sequence, n, m)[n][m]
+            if CurrentCmax < BestCmax:
                 best_sequence = temp_sequence
-                best_c_max = c_max_seq
+                BestCmax = CurrentCmax
         sequence = best_sequence
-    return int(best_c_max)
+    return int(BestCmax)
 
-def Neh(data, n, m):
-    #CTab = Cmax(data, n, m)
-    PTab = sumList(data, n, m)
-    current_sequence = []
-    current_sequence.append(PTab[0])
-    for i in range(1, n):
-        cm = float("inf")
-        temp = current_sequence
-        for j in range(0, i + 1):
-            temp_sequence = inserter(temp, j, PTab[i])#sekwencja,
-            temp_cmax = Cmax(data, len(temp_sequence), m)[len(temp_sequence)-1][m]
-            if cm > temp_cmax:
-                best_sequence = temp_sequence
-                cm = temp_cmax
-            current_sequence = best_sequence
-            print("i -", i, "| j -", j, "| temp seq-", temp_sequence, '| curr seq', current_sequence, "| best -", best_sequence, "| tempcmax -", temp_cmax)
-    return  current_sequence,
 
 def read_wyniki(nb):
     with open('data\\wyniki' + str(nb) + '.DAT') as f:
         return int(f.readline())
 
 if __name__ == '__main__':
-    lst = []
 
-    for i in range(1, 10):
-        tasks, n, m = ReadFile(i)
-
-        print(str(i) + " - " + str(neh(tasks, n, m)) + " - " + str(read_wyniki(i)))
+    wyniki_tab = []
+    result_tab = []
+    for file in range(1, 10):
+        data, n, m = ReadFile(file)
+        result = Neh(data, n, m)
+        result_tab.append(result)
+        wyniki_tab.append(read_wyniki(file))
+    print('wyniki obliczone: ')
+    print(result_tab)
+    print('wyniki ze strony: ')
+    print(wyniki_tab)
